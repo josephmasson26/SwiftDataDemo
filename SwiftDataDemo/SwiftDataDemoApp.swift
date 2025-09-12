f6d2cbbd887e41989c13f1e2aa5a17ed57ead756
josephmasson26 <c.p.joseph18@gmail.com>
2025-09-12 14:42:11 -0400

    Initial Commit

---
+++ SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Joseph Masson on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
